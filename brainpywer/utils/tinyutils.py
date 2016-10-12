445d53d873c10beb15d6d552ce657721dcbbbcde
Matt Gardner <matt@m-gardner.info>
2016-10-12 00:00:34 -0400

    First implemention of 'thaw' for snowflake id's
    Melt just sounded like a dumb function name.

---
+++ brainpywer/utils/tinyutils.py
@@ -0,0 +1,14 @@
+"""
+Home of all the miscellaneous BrainPywer utilities that don't deserve their own files
+"""
+
+
+def thaw(snowflake: int):
+    """
+    Tiny function to return the unix timestamp of a snowflake
+    :param snowflake: a discord snowflake (It's unique just like you! ha.)
+    :type  snowflake: int
+    :return: unix timestamp of the message
+    :rtype:  int
+    """
+    return ((snowflake >> 22)+1420070400000)/1000
